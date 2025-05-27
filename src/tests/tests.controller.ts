c8c22dbd1e26353ba5baa23fd24129edfa2e78c6
felibuscaglia <felipebbuscaglia@gmail.com>
2025-05-27 10:12:22 -0300

    Create test endpoint

--- src/tests/tests.controller.ts
+++ src/tests/tests.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { TestsService } from './tests.service';
 import { JwtAuthGuard } from 'auth/guards/auth.guard';
+import { CreateTestDto } from './dto';
+import { ProlificService } from 'prolific/prolific.service';
 
 @UseGuards(JwtAuthGuard)
 @Controller('tests')
 export class TestsController {
-  constructor(private readonly testsService: TestsService) {}
+  constructor(
+    private readonly testsService: TestsService,
+    private readonly prolificService: ProlificService,
+  ) {}
 
   @Get('/:id')
   getTestData(@Param('id') testId: string) {
     return this.testsService.getRawDataByTestId(testId);
+  }
+
+  @Post()
+  async createTest(@Body() dto: CreateTestDto) {
+    // Create the study in Prolific
+    const prolificStudy = await this.prolificService.createStudy(dto);
+
+    // Update the test variation in Supabase
+    await this.testsService.updateTestVariationStatus(
+      'draft',
+      dto.testId,
+      dto.variationType,
+      prolificStudy.id,
+    );
+
+    // Return the completion URL
+    return {
+      url: `https://app.prolific.com/submissions/complete?cc=${prolificStudy.completion_codes[0].code}`,
+    };
   }
 }
